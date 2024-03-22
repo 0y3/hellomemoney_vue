9de803a9ac5aae9959ea6a72434e256e30ef0c82
0y3 <trivin98@gmail.com>
2024-03-22 20:41:12 +0100

    tansfer

--- src/views/InternationalTransferView.vue
+++ src/views/InternationalTransferView.vue
@@ -7,17 +7,81 @@
   <section class="MC">
     <div class="auto-container">
       <div class="row justify-content-between py-5">
-        <div class="col-md-12 text-center text-white pb-5">
+        <div class="col-md-12 text-center text-white pb-2">
           <ul class="list1 clearfix list-flex">
             <li>International transfer</li>
           </ul>
         </div>
-        <CountryTransferComponent
-            :info="{
-              imgName: 'uk_r.png',
-              title: 'British pound',
-            }"
-          />
+        <div class="col-md-12">
+          <div class="image-box pb-4 text-center">
+                <figure class="image"><img src="/src/assets/img/flags/Africa.png" width="" alt=""></figure>
+          </div>
+          
+          
+          <ul class="list d-flex flex-wrap">
+           <CountryTransferComponent
+              :info="{
+                imgName: 'DZ.png',
+                name: 'Algeria',
+              }"
+           />
+           <CountryTransferComponent
+              :info="{
+                imgName: 'BW.png',
+                name: 'Botswana',
+              }"
+           />
+           <CountryTransferComponent
+              :info="{
+                imgName: 'BU.png',
+                name: 'Burundi',
+              }"
+           />
+           <CountryTransferComponent
+              :info="{
+                imgName: 'CA.png',
+                name: 'Cameroon',
+              }"
+           />
+           <CountryTransferComponent
+              :info="{
+                imgName: 'CV.png',
+                name: 'Cape Verde',
+              }"
+           />
+
+           <CountryTransferComponent
+              :info="{
+                imgName: 'KM.png',
+                name: 'Comoros',
+              }"
+           />
+           <CountryTransferComponent
+              :info="{
+                imgName: 'CD.png',
+                name: 'Cote D’ivoire',
+              }"
+           />
+           <CountryTransferComponent
+              :info="{
+                imgName: 'DJ.png',
+                name: 'Djibouti',
+              }"
+           />
+           <CountryTransferComponent
+              :info="{
+                imgName: 'DC.png',
+                name: 'Dr Congo',
+              }"
+           />
+           <CountryTransferComponent
+              :info="{
+                imgName: 'EG.png',
+                name: 'Egypt',
+              }"
+           />
+          </ul>
+        </div>
       </div>
     </div>
   </section>
@@ -27,8 +91,9 @@
 <style scoped>
 .MC{
   background: url('/src/assets/img/WorldMap3.png') no-repeat;
-  background-size: 100% 100%;
+  /* background-size: 100% 100%; */
 }
+
 .list1 li {
   position: relative;
   display: block;
